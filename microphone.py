459dcfd249c4c41b024272fc42828a0990ca50b9
PinkInk <tim.pelling@gmail.com>
2022-03-15 22:01:16 +0000

    cleanup

--- microphone.py
+++ microphone.py
@@ -1,5 +1,3 @@
-# https://www.st.com/resource/en/datasheet/mp34dt05-a.pdf
-# https://3cfeqx1hf82y3xcoull08ihx-wpengine.netdna-ssl.com/wp-content/uploads/2016/05/AN-000111-PDM-Decimation-v1.0.pdf
 
 import rp2
 import array
