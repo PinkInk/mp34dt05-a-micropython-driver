d18c88f9b5252929436a7431791327f7cfd34543
PinkInk <tim.pelling@gmail.com>
2022-03-19 23:01:44 +0000

    cleanup

--- microphone.py
+++ microphone.py
@@ -1,37 +1,17 @@
 import rp2
 import array
-# import micropython # not req'd to wrap assembler function(?)
 from machine import Pin
 from uctypes import addressof
 
-buf_len = 1024 # bytes
-buf = array.array('B', [0 for _ in range(buf_len)])
-pointer = 0
-
-# # count set bits in word
-# #   asm is several times faster 
-# #   than tested python options
-# @micropython.asm_thumb
-# def bcount(r0):
-#     mov(r1, 0)      # r1 = set bit counter = 0
-#     label(LOOP)
-#     cmp(r0, 0)      # value is, or is decremented to, 0?
-#     beq(END)        # goto END if it EQuals 0
-#     add(r1, 1)      # increment bit count
-#     mov(r2, r0)     # temp copy of value
-#     sub(r2, 1)      # subtracting 1 reverses LSB
-#     and_(r0, r2)    # remove LSB from value
-#     b(LOOP)         # goto LOOP
-#     label(END)
-#     mov(r0, r1)     # return count as r0
-
 # acquire pdm samples
 #   into 8 word FIFO
-clockspeed = 3_072_000 # 3.072e6
-steps = 8 # PIO clock steps per sample cycle
+clockspeed = 3_072_000 # PDM clock speed
+steps = 8 # PIO clock steps per PDM clock cycle
 pdm_clk = Pin(23)
 pdm_data = Pin(22)
+sample_buf = array.array('I', [0 for _ in range(8)])
 
+# join RX/TX 4 word buffers into one 8 word RX buffer
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW, out_init=rp2.PIO.IN_LOW, fifo_join=rp2.PIO.JOIN_RX)
 def sample():
     set(y, 8)                   # no. of word length samples
@@ -51,50 +31,44 @@ def sample():
     in_(pins, 1)
     set(pins, 0)
     push(noblock)               # push ISR to to RX FIFO
-    # push(block)
+
     jmp(y_dec, "WORDSTART")
     irq(rel(0))                 # raise irq - consume RX FIFO in main
 
-# irq handler
-#   retrieve 8 word FIFO into intermediate buffer
-#   count bits and stuff buffer 
-sample_buf = array.array('I', [0 for _ in range(8)])
 
-# def irq_handler(p):
-#     global sample_buf, pointer, pointer
-#     sm.get(sample_buf)
-#     # TODO: implement without mem alloc and enable hard irq
-#     #       then recheck timing
-#     buf[pointer] = sum(map(bcount, sample_buf))
-#     pointer += 1 if pointer<buf_len-1 else -pointer
+# sample buffer
+buf_len = 1024
+buf = array.array('B', [0 for _ in range(buf_len)])
 
-# data[0] = buffer length
-# data[1] = pointer
-# data[2] = address of start of buffer
+# buffer data wrapper
+#   data[0] = buffer length
+#   data[1] = index of current sample
+#   data[2] = address of start of buffer
 data = array.array('I', [buf_len, 0, addressof(buf)] )
 
 # r0 = sample_buf (8 word array)
 # r1 = data array
 @micropython.asm_thumb
 def push(r0, r1):
-    # r2 used throughout as overloaded scratch variable
+    # r2 = overloaded scratch variable
 
     # init
-    ldr(r3, [r1, 4])            # r3 = pointer
-    ldr(r4, [r1, 8])            # r4 = pointer to start of buffer
-    add(r4, r4, r3)             # r4 = pointer to current sample
+    ldr(r3, [r1, 4])            # r3 = get buf index (data[1])
+    ldr(r4, [r1, 8])            # r4 = address of start of buffer (data[2])
+    add(r4, r4, r3)             # add buf index
 
     # sample buffer loop (SBL)
-    mov(r5, 0)                  # r5 = current sample running bit set count
-    mov(r6, 0)                  # r6 = 8 word sample buffer pointer
-    label(SBL_START)            # sample buffer loop START
-    cmp(r6, 32)
-    beq(SBL_END)
+    mov(r5, 0)                  # r5 = current sample running set-bit count
+    mov(r6, 0)                  # r6 = init pointer into 8 word sample_buf
+    label(SBL_START)            # sample_buf loop START
+    cmp(r6, 32)                 # 8 * 4 byte words = 32 bits
+    beq(SBL_END)                # end of buffer? goto: sample_buf loop END
 
     # sample loop
-    mov(r2, r0)                 # load addressof sample_buf
+    mov(r2, r0)                 # r2 = address of sample_buf
     add(r2, r2, r6)             # add sample_buf pointer
-    ldr(r7, [r2, 0])            # load the current sample
+    ldr(r7, [r2, 0])            # r7 = current sample
+
     label(SL_START)             # sample loop START
     cmp(r7, 0)
     beq(SL_END)
@@ -103,8 +77,8 @@ def push(r0, r1):
     sub(r2, 1)                  # subtract 1 reverses LSB
     and_(r7, r2)                # remove LSB from sample
     b(SL_START)
-    label(SL_END)               # sample loop END
 
+    label(SL_END)               # sample loop END
     add(r6, 4)                  # increment sample counter one word
     label(SBL_END)              # sample buffer loop END
 
@@ -122,7 +96,6 @@ def push(r0, r1):
 
 
 def irq_handler(p):
-    # global data, sample_buf
     sm.get(sample_buf)
     # push(sample_buf, data)
 
@@ -131,12 +104,7 @@ sm = rp2.StateMachine(0, sample, freq=clockspeed*steps, set_base=pdm_clk, in_bas
 sm.irq(handler=irq_handler) #, hard=True)
 sm.active(True)
 
-# # time filling buffer ~= 0.28s
-# # whereas I expect ~= 0.085s
-# # i.e. around 3.2 x slower than expected?
-# # 
-# # must be the irq_handler since replacing it with 
-# # a basic counter achieves expected timing
+# # timing test
 # import time
 # time.sleep(1) # wait for statemachine to initialise
 # st = time.ticks_us()
