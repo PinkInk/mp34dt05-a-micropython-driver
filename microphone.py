500f1e7a7ea69f749ca425155256ccf05067dc2e
PinkInk <tim.pelling@gmail.com>
2022-03-20 08:18:08 +0000

    sample decimate & store working, and cleanup/annotate

--- microphone.py
+++ microphone.py
@@ -3,15 +3,23 @@ import array
 from machine import Pin
 from uctypes import addressof
 
-# acquire pdm samples
-#   into 8 word FIFO
-clockspeed = 3_072_000 # PDM clock speed
+clockspeed = 3_072_000 # PDM clock frequency
 steps = 8 # PIO clock steps per PDM clock cycle
 pdm_clk = Pin(23)
 pdm_data = Pin(22)
 sample_buf = array.array('I', [0 for _ in range(8)])
 
-# join RX/TX 4 word buffers into one 8 word RX buffer
+# sample buffer
+buf_len = 1024
+buf = array.array('B', [0 for _ in range(buf_len)])
+
+# sample buffer wrapper
+#   data[0] = buffer length
+#   data[1] = index of current sample
+#   data[2] = address of start of buffer
+data = array.array('I', [buf_len, 0, addressof(buf)] )
+
+# sample PDM microphone using PIO
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW, out_init=rp2.PIO.IN_LOW, fifo_join=rp2.PIO.JOIN_RX)
 def sample():
     set(y, 8)                   # no. of word length samples
@@ -35,19 +43,10 @@ def sample():
     jmp(y_dec, "WORDSTART")
     irq(rel(0))                 # raise irq - consume RX FIFO in main
 
-
-# sample buffer
-buf_len = 1024
-buf = array.array('B', [0 for _ in range(buf_len)])
-
-# buffer data wrapper
-#   data[0] = buffer length
-#   data[1] = index of current sample
-#   data[2] = address of start of buffer
-data = array.array('I', [buf_len, 0, addressof(buf)] )
-
-# r0 = sample_buf (8 word array)
-# r1 = data array
+# count bits in 8 word sample and store in buffer
+# python variants take longer than the sampling period
+#   r0 = sample_buf (8 word array)
+#   r1 = data array
 @micropython.asm_thumb
 def push(r0, r1):
     # r2 = overloaded scratch variable
@@ -87,33 +86,34 @@ def push(r0, r1):
     # store sample set-bit count into buf[index]
     strb(r5, [r4, 0])           # buf is a Byte array
 
-    # TODO: this is not incrementing buf index!
-    # increment buf index
+    # increment and store buf index
     ldr(r2, [r1, 0])            # r2 = buf_len
     add(r3, 1)                  # increment     
     cmp(r3, r2)                 # index = buf_len?
-    beq(SKIP_RESET)             #   GOTO: SKIP_RESET
+    bne(SKIP_RESET)             #   GOTO: SKIP_RESET
     mov(r3, 0)                  # re-init index = 0
     label(SKIP_RESET)
     str(r3, [r1, 4])            # store buf index back to data
     
     mov(r0, r2)
 
-
+# get samples and store in buffer
+#   p = irq (passed by StateMachine.irq)
 def irq_handler(p):
     sm.get(sample_buf)
-    print(push(sample_buf, data), end=' ')
+    push(sample_buf, data)
 
 # init and start the statemachine
 sm = rp2.StateMachine(0, sample, freq=clockspeed*steps, set_base=pdm_clk, in_base=pdm_data)
+# hard interupt flag causes lockup?
 sm.irq(handler=irq_handler) #, hard=True)
 sm.active(True)
 
 # # timing test
 # import time
-# time.sleep(1) # wait for statemachine to initialise
+# # time.sleep(1) # wait for statemachine to initialise
 # st = time.ticks_us()
-# sp = pointer
-# while pointer != sp: 
+# sp = data[1]
+# while data[1] != sp: 
 #     pass
 # f'{time.ticks_diff(time.ticks_us(), st)/1e6:.4f} seconds'
